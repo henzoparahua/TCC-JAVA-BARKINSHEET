e730db8f4f0791bdb621aa7b421bc1f0cb1314dc
henzoparahua <hnz.2894henzop19@gmail.com>
2023-08-16 20:23:49 -0300

    coisas

--- src/main/java/com/helpfox/main/Controller/viewControllers/LogInController.java
+++ src/main/java/com/helpfox/main/Controller/viewControllers/LogInController.java
@@ -4,11 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class LogInController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+
+
 }
