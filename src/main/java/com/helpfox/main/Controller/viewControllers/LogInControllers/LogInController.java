4967c1431ddcdee24d81f3ffa2807bf328f64e94
henzoparahua <hnz.2894henzop19@gmail.com>
2023-08-21 00:50:01 -0300

    consegui :D amanha eu continuo. Acho que estou me cobrando pouco demais de uns tempos pra cá. Preciso me cobrar mais.

--- src/main/java/com/helpfox/main/Controller/viewControllers/LogInControllers/LogInController.java
+++ src/main/java/com/helpfox/main/Controller/viewControllers/LogInControllers/LogInController.java
@@ -1,9 +1,11 @@
 package com.helpfox.main.Controller.viewControllers.LogInControllers;
 
 import com.helpfox.main.Controller.viewControllers.ReusableViewController;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
@@ -19,13 +21,20 @@ public class LogInController implements Initializable {
     @FXML
     private StackPane fatherStackPane;
     @FXML
-    private VBox SignInLoaderFXML;
-
+    private VBox loginContainer;
+    @FXML
+    private VBox loginContext;
+    @FXML
+    private Button btReturn;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        setSignInLoaderFXML();
         buttonBarFXMLLoader();
+        try {
+            signUp();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
@@ -47,17 +56,43 @@ public class LogInController implements Initializable {
             e.printStackTrace();
         }
     }
-    final void setSignInLoaderFXML(){
-        try {
-            FXMLLoader SignInLoader = new FXMLLoader(getClass()
-                    .getResource("/com/helpfox/main/View/fxml/LogInResources/SignInResources.fxml"));
-            SignInLoaderFXML = SignInLoader.load();
-            fatherStackPane.getChildren().add(SignInLoaderFXML);
-            SignInController signInController = SignInLoader.getController();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
 
+    final void signUp() throws IOException {
+        FXMLLoader SignUpContextLoader = new FXMLLoader(getClass()
+                .getResource("/com/helpfox/main/View/fxml/LogInResources/SignUpResources.fxml"));
+        loginContext = SignUpContextLoader.load();
+
+        // Verifica a existencia do node.
+        if (loginContainer.getChildren().contains(loginContext)) {
+            int indexOfChild = loginContainer.getChildren().indexOf(loginContext);
+            loginContainer.getChildren().set(indexOfChild, loginContext);
+        } else {
+            // Adiciona a children caso não exista.
+            loginContainer.getChildren().add(1, loginContext);
+        }
+            // Controller
+        SignUpResources signUpResources = SignUpContextLoader.getController();
+    }
+    final void signIn() throws IOException {
+        FXMLLoader SignUpContextLoader = new FXMLLoader(getClass()
+                .getResource("/com/helpfox/main/View/fxml/LogInResources/SignInResources.fxml"));
+        loginContext = SignUpContextLoader.load();
+
+        // Verifica a existencia do node.
+        if (loginContainer.getChildren().contains(loginContext)) {
+            int indexOfChild = loginContainer.getChildren().indexOf(loginContext);
+            loginContainer.getChildren().set(indexOfChild, loginContext);
+        } else {
+            // Adiciona a children caso não exista.
+            loginContainer.getChildren().add(1, loginContext);
+        }
+        // Controller
+        SignUpResources signUpResources = SignUpContextLoader.getController();
+    }
+    @FXML
+    void returnOnClick(ActionEvent event) throws IOException {
+        loginContainer.getChildren().remove(loginContext);
+        signIn();
     }
 }
 
