8534ff7f30eab251a0ecfac6b1c5d754034a6f47
henzoparahua <hnz.2894henzop19@gmail.com>
2023-08-30 17:52:36 -0300

    wip Menu GUI

---
+++ src/main/java/com/helpfox/main/Controller/GUI/Menu/Components/VehicleCellController.java
@@ -0,0 +1,29 @@
+package com.helpfox.main.Controller.GUI.Menu.Components;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class VehicleCellController implements Initializable {
+
+    @FXML
+    public Label numCell;
+    @FXML
+    public Label nameCell;
+    @FXML
+    public Button btPlateOne;
+    @FXML
+    public Button btPlateTwo;
+    @FXML
+    public Button btPlateThree;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+    }
+}
