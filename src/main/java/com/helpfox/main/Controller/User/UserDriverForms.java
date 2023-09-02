1a7a81898982fcc9995670d19c5e3cc7173a9bcf
henzoparahua <hnz.2894henzop19@gmail.com>
2023-09-01 21:55:28 -0300

    wip Menu GUI

--- src/main/java/com/helpfox/main/Controller/User/UserDriverForms.java
+++ src/main/java/com/helpfox/main/Controller/User/UserDriverForms.java
@@ -1,4 +1,27 @@
 package com.helpfox.main.Controller.User;
 
-public class UserDriverForms {
+import com.helpfox.main.Model.Model;
+import com.helpfox.main.View.ViewFactory;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UserDriverForms implements Initializable {
+    @FXML
+    private Button btConfirm;
+    @FXML
+    private Button btCancel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        btCancel.setOnAction(event -> {
+            Stage stage = (Stage) btCancel.getScene().getWindow();
+            Model.getInstance().getViewFactory().closeStage(stage);
+        });
+    }
 }
---
+++ src/main/java/com/helpfox/main/View/DriverCellFactory.java
@@ -0,0 +1,27 @@
+package com.helpfox.main.View;
+
+import com.helpfox.main.Controller.User.DriverCell;
+import com.helpfox.main.Model.Vehicle.Vehicle;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ListCell;
+
+public class DriverCellFactory extends ListCell<Vehicle> {
+    @Override
+    protected void updateItem(Vehicle vehicle, boolean empty) {
+        super.updateItem(vehicle, empty);
+        if (empty){
+            setText(null);
+            setGraphic(null);
+        } else {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXMLs/isUser/driverCell.fxml"));
+            DriverCell controller = new DriverCell(vehicle);
+            loader.setController(controller);
+            setText(null);
+            try {
+                setGraphic(loader.load());
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
