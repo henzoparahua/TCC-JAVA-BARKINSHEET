b4cf0806548a051f09120caca1ea65976c4c48b6
pedroramos17 <pedro_ramos2003@hotmail.com>
2023-09-08 15:00:14 -0300

    add verifyUser function in CheckingAccount

--- src/main/java/com/helpfox/main/Model/CheckingAccount.java
+++ src/main/java/com/helpfox/main/Model/CheckingAccount.java
@@ -1,7 +1,21 @@
 package com.helpfox.main.Model;
 
 import com.helpfox.main.Model.User.User;
+import com.helpfox.main.Model.User.UserDAO;
+import com.helpfox.main.Model.User.UserSearchType;
 
-public class CheckingAccount extends User {
+import java.util.List;
 
+public class CheckingAccount {
+    private UserDAO userDAO;
+
+    public CheckingAccount(UserDAO userDAO) { this.userDAO = userDAO; }
+
+    public Boolean verifyUser(String email) {
+        List<User> user = userDAO.findByProp(UserSearchType.EMAIL, email);
+
+        if(!user.isEmpty()) {
+            return true;
+        } else { return false; }
+    }
 }
