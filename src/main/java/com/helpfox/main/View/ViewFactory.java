3cbc7c734c58f7a3a16710b24f8d552c6c47b17a
henzoparahua <hnz.2894henzop19@gmail.com>
2023-09-08 04:17:54 -0300

    topbar 2/4

--- src/main/java/com/helpfox/main/View/ViewFactory.java
+++ src/main/java/com/helpfox/main/View/ViewFactory.java
@@ -2,7 +2,6 @@ package com.helpfox.main.View;
 
 import com.helpfox.main.App;
 import com.helpfox.main.Controller.User.UserController;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -15,6 +14,7 @@ import javafx.stage.StageStyle;
 import java.io.IOException;
 
 public class ViewFactory {
+
     // Client View
     private AnchorPane dashboardView;
 
