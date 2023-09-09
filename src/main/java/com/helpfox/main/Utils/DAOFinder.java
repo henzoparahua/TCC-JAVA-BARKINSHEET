42e1c3fa5953fd2d7879706e6ec449e905e08b4a
pedroramos17 <pedro_ramos2003@hotmail.com>
2023-09-09 19:36:16 -0300

    refactor: optimize class loader in DAOFinder util

--- src/main/java/com/helpfox/main/Utils/DAOFinder.java
+++ src/main/java/com/helpfox/main/Utils/DAOFinder.java
@@ -8,16 +8,13 @@ public class DAOFinder {
         public static List<Object> daoInstances = new ArrayList<>();
         public static String pkgName = "com.helpfox.main.Model.SQLite";
     public static List<Object> buildDAO(List<String> daoClassNames) {
-
         try {
             ClassLoader classLoader = DAOFinder.class.getClassLoader();
-            Package pkg = classLoader.getDefinedPackage(pkgName);
 
             for (String daoClassName : daoClassNames) {
                 try {
-                    Class<?> clazz = Class.forName(pkgName + "." + daoClassName);
-                    Constructor<?> constructor = clazz.getDeclaredConstructor();
-                    Object daoInstance = constructor.newInstance();
+                    Class<?> clazz = classLoader.loadClass(pkgName + "." + daoClassName);
+                    Object daoInstance = clazz.getDeclaredConstructor().newInstance();
                     daoInstances.add(daoInstance);
                 } catch (ClassNotFoundException e) {
                     System.err.println("Class not found: " + daoClassName);
@@ -36,15 +33,13 @@ public class DAOFinder {
             ClassLoader classLoader = DAOFinder.class.getClassLoader();
 
             // Use the class loader to load all classes in the package
-            Package pkg = classLoader.getDefinedPackage(pkgName);
-            Class<?>[] classes = Class.forName(pkg.toString()).getDeclaredClasses();
+            Class<?>[] classes = classLoader.loadClass(pkgName).getDeclaredClasses();
 
             // Iterate through the classes and instantiate DAOs
             for (Class<?> clazz : classes) {
                 // Check if the class name ends with "DAO"
                 if (clazz.getSimpleName().endsWith("DAO")) {
-                    Constructor<?> constructor = clazz.getDeclaredConstructor();
-                    Object daoInstance = constructor.newInstance();
+                    Object daoInstance = clazz.getDeclaredConstructor().newInstance();
                     daoInstances.add(daoInstance);
                 }
             }
