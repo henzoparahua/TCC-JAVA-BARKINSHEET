68a8f9ee21e88d854fb6e6df82cd2f3520ecb589
pedroramos17 <pedro_ramos2003@hotmail.com>
2023-09-10 18:43:44 -0300

    config: add init func to first config on project

--- src/main/java/com/helpfox/main/App.java
+++ src/main/java/com/helpfox/main/App.java
@@ -3,11 +3,44 @@ package com.helpfox.main;
 import com.helpfox.main.Model.Model;
 import javafx.application.Application;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
+
+import static com.helpfox.main.Utils.DBUtils.*;
 
 public class App extends Application {
+
+    @Override
+    public void init() throws Exception {
+        try {
+            if (configFileExists()) {
+                System.out.println("Running config setup...");
+            } else {
+                // Create the configuration file first
+                createConfigFile();
+
+                List<String> databaseFileNames = List.of("users.db", "drivers.db", "vehicles.db");
+
+                if (!allDatabaseExists(databaseFileNames)) {
+                    setSetupCompleted(false);
+                }
+                if (!isSetupCompleted()) {
+                    System.out.println("Running database setup...");
+
+                    setupDatabase();
+
+                    // Update configuration to indicate setup completion
+                    setSetupCompleted(true);
+                } else {
+                    // Setup has already been completed
+                    System.out.println("Database setup has already been completed.");
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Initial setup hasn't already been completed.");
+        }
+    }
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -16,7 +49,6 @@ public class App extends Application {
 
     public static void main(String[] args) throws SQLException {
         launch(args);
-
 
 //      The code below start a local database test, if there is any error, comment start function and/or launch(args)
 
