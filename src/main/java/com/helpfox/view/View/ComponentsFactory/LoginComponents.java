da42d83ac165b5f71b645469e3a9e96f7af45f0e
henzoparahua <hnz.2894henzop19@gmail.com>
2023-09-26 22:04:08 -0300

    initial commit reworking gui for api

--- src/main/java/com/helpfox/view/View/ComponentsFactory/LoginComponents.java
+++ src/main/java/com/helpfox/view/View/ComponentsFactory/LoginComponents.java
@@ -2,9 +2,14 @@ package com.helpfox.view.View.ComponentsFactory;
 
 import com.helpfox.view.View.LoginFactory;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 
 public class LoginComponents {
     private static LoginComponents contentLogin;
@@ -17,16 +22,32 @@ public class LoginComponents {
     public VBox verticalBox(){
         VBox vBox= new VBox();
         Label label = new Label();
-        ImageView imageView = new ImageView();
-        TextField passwordField = new TextField();
-        vBox.getChildren().addAll(label, imageView, textField(), passwordField);
-        return vBox;
-    }
-    private TextField textField(){
+        Image image = new Image(getClass().getResourceAsStream("../../main/Images/barkinsheetlogo.png"));
+        ImageView imageView = new ImageView(image);
+
+        label.setText("Barkin");
+        label.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
+
         TextField usernameField = new TextField();
-        usernameField.setMinWidth(200.0);
-        usernameField.setPrefWidth(200.0);
-        usernameField.setMaxWidth(200.0);
-        return textField();
+
+        usernameField.setMaxHeight(30);
+        usernameField.setMinHeight(30);
+        usernameField.setPrefHeight(30);
+        usernameField.setMinWidth(200);
+        usernameField.setPrefWidth(200);
+        usernameField.setMaxWidth(200);
+
+        PasswordField passwordField = new PasswordField();
+
+        passwordField.setMaxHeight(30);
+        passwordField.setMinHeight(30);
+        passwordField.setPrefHeight(30);
+        passwordField.setMinWidth(200);
+        passwordField.setPrefWidth(200);
+        passwordField.setMaxWidth(200);
+
+
+        vBox.getChildren().addAll(label, imageView, usernameField, passwordField);
+        return vBox;
     }
 }
