94851d4b9391b7fec087436cdb6140e2ab293eb6
henzoparahua <hnz.2894henzop19@gmail.com>
2023-09-28 23:38:03 -0300

    wip login gui

--- src/main/java/com/helpfox/view/View/ComponentsFactory/LoginFactory.java
+++ src/main/java/com/helpfox/view/View/ComponentsFactory/LoginFactory.java
@@ -1,11 +1,14 @@
 package com.helpfox.view.View.ComponentsFactory;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
@@ -21,42 +24,50 @@ public class LoginFactory {
         return contentLogin;
     }
     public void showLogin(StackPane root){
-        VBox vBoxLogin = LoginFactory.getInstance().verticalBox();
-        root.setAlignment(vBoxLogin, Pos.TOP_LEFT);
+        VBox vbox = LoginFactory.getInstance().verticalBox(root);
+        root.setAlignment(vbox, Pos.TOP_LEFT);
         root.getStylesheets().addAll("/com/helpfox/main/Styles/DarkTheme/LoginStylesheet.css");
         root.setId("mainContainer");
-        root.getChildren().add(vBoxLogin);
+        root.getChildren().add(vbox);
     }
-    public VBox verticalBox(){
-        VBox vBox= new VBox();
-        vBox.setId("loginContainer");
+    public VBox footerBox(){
+        VBox vbox = new VBox();
+        Button forgotButton = new Button();
+        forgotButton.setText("Esqueceu sua senha?");
+        vbox.setAlignment(Pos.BOTTOM_CENTER);
+        vbox.getChildren().add(forgotButton);
+        forgotButton.setId("forgotPswrd");
+        return vbox;
+    }
+    public VBox verticalBox(StackPane root){
+        VBox vbox= new VBox();
+        vbox.setId("loginContainer");
+        vbox.getChildren().addAll(vBox(), footerBox());
+
+        vBox().setAlignment(Pos.TOP_CENTER);
+
+        return vbox;
+    }
+    public VBox vBox(){
+        VBox vBox = new VBox();
         Label label = new Label();
-        Image image = new Image(getClass().getResourceAsStream("/com/helpfox/main/Images/barkinsheetlogo-min.png"));
+        Image image = new Image(getClass().getResourceAsStream("/com/helpfox/main/Images/barkinsheetlogo.png"));
         ImageView imageView = new ImageView(image);
-        vBox.setAlignment(Pos.TOP_CENTER);
-        label.setText("Barkin");
-        label.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
-
+        imageView.setId("imageBarkin");
+        label.setText("FAZER LOGIN");
+        imageView.setFitWidth(160.0);
+        imageView.setFitHeight(160.0);
         TextField usernameField = new TextField();
-
-        usernameField.setMaxHeight(30);
-        usernameField.setMinHeight(30);
-        usernameField.setPrefHeight(30);
-        usernameField.setMinWidth(200);
-        usernameField.setPrefWidth(200);
-        usernameField.setMaxWidth(200);
-
         PasswordField passwordField = new PasswordField();
-
-        passwordField.setMaxHeight(30);
-        passwordField.setMinHeight(30);
-        passwordField.setPrefHeight(30);
-        passwordField.setMinWidth(200);
-        passwordField.setPrefWidth(200);
-        passwordField.setMaxWidth(200);
-
-
-        vBox.getChildren().addAll(label, imageView, usernameField, passwordField);
+        Button loginButton = new Button();
+        loginButton.setText("ENTRAR");
+        vBox.setAlignment(Pos.TOP_CENTER);
+        vBox.setSpacing(12.0);
+        vBox.getChildren().addAll(imageView, label, usernameField, passwordField, loginButton);
+        VBox.setMargin(imageView, new Insets(35.5, 0, 20, 0));
+        VBox.setMargin(label, new Insets(0, 0, 15, 0));
+        VBox.setMargin(loginButton, new Insets(10.5, 0, 0, 0));
+        vBox.setId("vboxForms");
         return vBox;
     }
 }
